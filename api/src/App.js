fbb68420a01f1632cd746bb1def9ccfa89618e0c
Kyla Heart Pineda <125124933+pkylaheart@users.noreply.github.com>
2026-05-05 08:07:38 +0800

    updated app

--- api/src/App.js
+++ api/src/App.js
@@ -5,23 +5,16 @@ function App() {
   const [weather, setWeather] = useState(null);
   const [error, setError] = useState("");
 
-  const API_KEY = process.env.REACT_APP_API_KEY;
-  console.log("API KEY:", API_KEY);
+  const API_KEY = "e2c9eb509823b62d7f20fdf9ed11e978";
+
   const getWeather = async () => {
     try {
       setError("");
-      setWeather(null);
 
       const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
       );
 
-      // Handle rate limit
-      if (response.status === 429) {
-        throw new Error("Too many requests. Try again later.");
-      }
-
-      // Handle invalid city
       if (!response.ok) {
         throw new Error("City not found");
       }
@@ -31,39 +24,32 @@ function App() {
 
     } catch (err) {
       setError(err.message);
+      setWeather(null);
     }
   };
 
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>🌦️ Weather App</h1>
+    <div>
+      <h1>Weather App</h1>
 
       <input
         type="text"
         placeholder="Enter city"
         value={city}
         onChange={(e) => setCity(e.target.value)}
-        style={{ padding: "10px", width: "200px" }}
       />
 
-      <br /><br />
+      <button onClick={getWeather}>Get Weather</button>
 
-      <button onClick={getWeather} style={{ padding: "10px 20px" }}>
-        Get Weather
-      </button>
+      {error && <p>{error}</p>}
 
-      <div style={{ marginTop: "20px" }}>
-        {error && <p style={{ color: "red" }}>{error}</p>}
-
-        {weather && (
-          <div>
-            <h2>{weather.name}</h2>
-            <p>🌡 Temperature: {weather.main.temp} °C</p>
-            <p>🌥 Condition: {weather.weather[0].description}</p>
-            <p>💧 Humidity: {weather.main.humidity}%</p>
-          </div>
-        )}
-      </div>
+      {weather && (
+        <div>
+          <h2>{weather.name}</h2>
+          <p>Temperature: {weather.main.temp} °C</p>
+          <p>Condition: {weather.weather[0].description}</p>
+        </div>
+      )}
     </div>
   );
 }
